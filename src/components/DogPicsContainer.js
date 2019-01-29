fa3be8f01f33846ade58204dd0e556297bb5b4d3
crvicek <crvicek@yahoo.com>
2019-01-29 11:42:24 +0100

    MVP 0.2

--- src/components/DogPicsContainer.js
+++ src/components/DogPicsContainer.js
@@ -2,6 +2,8 @@ import * as React from 'react'
 import * as request from 'superagent'
 import DogPics from './DogPics'
 
+const random = Math.floor(Math.random() * 87)
+
 
 export default class DogPicsContainer extends React.Component {
   state = {
@@ -10,7 +12,7 @@ export default class DogPicsContainer extends React.Component {
     }
   
   getImages () {
-    request(`https://dog.ceo/api/breed/${Object.keys(this.state.breeds)[2]}/images`)
+    request(`https://dog.ceo/api/breed/${Object.keys(this.state.breeds)[random]}/images`)
     // .then(response => this.setState(Object.keys(this.state)[2]['images'][response]))
 
     // .then(response => Object.keys(this.state)[2][response])
@@ -42,16 +44,17 @@ updateImages(images) {
   }
 
 
-
   render() {
     console.log(this.state)
     if (!this.state) return 'Loading...'
     return (
       <div>
     <h1>we have {Object.keys(this.state.breeds).length} breeds</h1>
-    <h2>This is {Object.keys(this.state.breeds)[2]}</h2>
+    <h2>Is this {Object.keys(this.state.breeds)[random]}?</h2>
+    <h2>Is this {Object.keys(this.state.breeds)[Math.floor(Math.random() * 87)]}?</h2>
+    <h2>Is this {Object.keys(this.state.breeds)[Math.floor(Math.random() * 87)]}?</h2>
     {/* <img src='https://images.dog.ceo/breeds/airedale/n02096051_1111.jpg' alt='img'></img> */}
-    <img src={this.state.img[Math.floor(Math.random()*100)]} alt='img'></img>
+    <img src={this.state.img[Math.floor(Math.random()*this.state.img.length)]} alt='img'></img>
     {/* <h2>THis is {Object.keys(this.state)[Math.floor(Math.random() * 87)]}</h2> */}
 
     {/* <DogPics dogs={this.state.dogs} /> */}
