739f68e19e2868659fc5c0075528e55855557849
crvicek <crvicek@yahoo.com>
2019-01-29 11:22:28 +0100

    mvp fetch breed and pic working

--- src/components/DogPicsContainer.js
+++ src/components/DogPicsContainer.js
@@ -5,20 +5,57 @@ import DogPics from './DogPics'
 
 export default class DogPicsContainer extends React.Component {
   state = {
-    
+    img: [],
+    breeds: {}
+    }
+  
+  getImages () {
+    request(`https://dog.ceo/api/breed/${Object.keys(this.state.breeds)[2]}/images`)
+    // .then(response => this.setState(Object.keys(this.state)[2]['images'][response]))
+
+    // .then(response => Object.keys(this.state)[2][response])
+    .then(data => this.updateImages(data.body.message))
+    .catch(console.error)
   }
+
+updateImages(images) {
+  this.setState({ img: images })
+
+// this.setState({ img: images })
+
+}
+  
 
   componentDidMount() {
     request('https://dog.ceo/api/breeds/list/all')
       // .then(response => console.log(response.body.message))
-      .then(response => this.setState(response.body.message))
+      .then(response => this.updateBreeds(response.body.message))
+      // .then(it => console.log(it))
+      .then(()=>this.getImages())
   }
 
+  updateBreeds(breed) {
+    this.setState({ breeds: breed })
+  
+  // this.setState({ img: images })
+  
+  }
+
+
+
   render() {
+    console.log(this.state)
     if (!this.state) return 'Loading...'
     return (
-    <h1>we have {this.state.length} dogs</h1>
-    // <DogPics dogs={this.state.dogs} />
+      <div>
+    <h1>we have {Object.keys(this.state.breeds).length} breeds</h1>
+    <h2>This is {Object.keys(this.state.breeds)[2]}</h2>
+    {/* <img src='https://images.dog.ceo/breeds/airedale/n02096051_1111.jpg' alt='img'></img> */}
+    <img src={this.state.img[Math.floor(Math.random()*100)]} alt='img'></img>
+    {/* <h2>THis is {Object.keys(this.state)[Math.floor(Math.random() * 87)]}</h2> */}
+
+    {/* <DogPics dogs={this.state.dogs} /> */}
+    </div>
     )
 
   }
