8c571e2c564749a5d5602428c9d1d440fbfe104b
LeoniePeters <leoniepeters@gmail.com>
2019-01-30 16:02:48 +0100

    move ordering loops to localstate

--- src/components/DogPics.js
+++ src/components/DogPics.js
@@ -1,10 +1,10 @@
 import *  as React from 'react';
 
-const arr = []
-while(arr.length < 3){
-    var r = Math.floor(Math.random()*10);
-    if(arr.indexOf(r) === -1) arr.push(r);
-} 
+// const arr = []
+// while(arr.length < 3){
+//     var r = Math.floor(Math.random()*10);
+//     if(arr.indexOf(r) === -1) arr.push(r);
+// } 
 
 const orderButton = []
 while(orderButton.length <= 2){
@@ -20,11 +20,14 @@ export default function DogPics(props) {
       {props.answer}
 <div className="AllOptions">
   <button className="Option" onClick={props.handleCorrect} 
-  style={{backgroundColor: props.localState.backgroundColor1, order: orderButton[0]}}>{props.current}</button>
+  style={{backgroundColor: props.localState.backgroundColor1, order: props.localState.buttonOrder[0]}}>
+  {props.current}</button>
   <button className="Option" onClick={props.handleWrong1}
-  style={{backgroundColor: props.localState.backgroundColor2, order: orderButton[1]}}>{Object.keys(props.allbreeds)[arr[1]]}</button>
+  style={{backgroundColor: props.localState.backgroundColor2, order: props.localState.buttonOrder[1]}}>
+  {Object.keys(props.allbreeds)[props.localState.dogName[1]]}</button>
   <button className="Option" onClick={props.handleWrong2}
-  style={{backgroundColor: props.localState.backgroundColor3, order: orderButton[2]}}>{Object.keys(props.allbreeds)[arr[2]]}</button>
+  style={{backgroundColor: props.localState.backgroundColor3, order: props.localState.buttonOrder[2]}}>
+  {Object.keys(props.allbreeds)[props.localState.dogName[2]]}</button>
   </div>
   </div>
   )}
--- src/components/DogPicsContainer.js
+++ src/components/DogPicsContainer.js
@@ -4,16 +4,43 @@ import { setBreeds } from '../actions/setBreeds'
 import { connect } from 'react-redux'
 import DogPics from './DogPics'
 
+const arr = []
+while(arr.length < 3){
+var r = Math.floor(Math.random()*10);
+if(arr.indexOf(r) === -1) arr.push(r);
+}
+const arr1 = []
+while(arr1.length < 3){
+var s = Math.floor(Math.random()*10);
+if(arr1.indexOf(s) === -1) arr1.push(s);
+}
+
+const orderButton = []
+while(orderButton.length <= 2){
+    var q = Math.floor(Math.random()*3);
+    if(orderButton.indexOf(q) === -1) orderButton.push(q);
+} 
+const orderButton1 = []
+while(orderButton1.length <= 2){
+    var o = Math.floor(Math.random()*3);
+    if(orderButton1.indexOf(o) === -1) orderButton1.push(o);
+} 
+
 class DogPicsContainer extends React.Component {
   state = {
     backgroundColor1: 'rgb(144, 191, 231)',
     backgroundColor2: 'rgb(144, 191, 231)',
-    backgroundColor3: 'rgb(144, 191, 231)'
+    backgroundColor3: 'rgb(144, 191, 231)',
+    dogName: arr,
+    buttonOrder: orderButton
     }
-
+  
   handleCorrect = () => {
     console.log('Correct!')
-    this.setState({backgroundColor1: 'green'})
+    this.setState({backgroundColor1: 'green',
+    dogName: arr ? arr1 : arr,
+    buttonOrder: orderButton ? orderButton1 : orderButton})
+
   }
 
   handleWrong1 = () => {
@@ -31,6 +58,7 @@ class DogPicsContainer extends React.Component {
   }
 
   render() {
+    console.log(this.state.dogName)
     // console.log(this.props);
     if (!this.props.allbreeds) return 'Loading...'
     return (
